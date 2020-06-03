dc7e65a2989ebbf4dca0e91c133a2ca19e3a13bc
aidesign-hcm <aithietke@gmail.com>
2020-06-03 16:02:18 +0700

    first Push

---
+++ components/productCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-col md="3" cols="12">
+    <v-card class="mx-auto">
+      <v-img
+        class="white--text align-end"
+        :src="product.productImage"
+      >
+    
+        <v-card-title></v-card-title>
+      </v-img>
+      <v-card-subtitle class="pb-0">Owner: {{product.owner.name}}</v-card-subtitle>
+      <v-card-text class="text--primary">
+        <h2><nuxt-link :to="/products/ + (product.id)" >{{product.title}}</nuxt-link></h2>
+        <span>{{product.description}}</span>
+        <div>Type: {{product.category.type}}</div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="orange" text>
+         Price:  {{product.price}}
+        </v-btn>
+        <v-btn :to="/products/ + (product.id)" color="orange" text>
+            Read More
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+    name: 'productCard',
+    props: {
+    product: {
+        type: Object,
+        default: function () {
+        return {
+             owner:{
+                 type: Object,
+                 default:""
+            },
+            category:{
+                 type: Object,
+                 default:""
+            }
+         }
+      }
+    }
+    },
+};
+</script>
+
+<style></style>
---
+++ pages/products/_id.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-container class="mb-6">
+        <v-row >
+            <v-col md="5" cols="12">
+                <v-img
+                    class="white--text align-end"
+                    :src="product.photo"
+                />
+            </v-col>
+            <v-col md="4" cols="12">
+                <h2 class="title">{{product.title}}</h2>
+                <p class="description">{{product.description}}</p>
+                <h4>Price: {{product.price}} VNĐ</h4>
+                <i>Category: {{product.category.type}}</i>
+                <i>Owner: {{product.owner.name}}</i>
+                <p>Stock: {{product.StockQuantity}}</p>
+            </v-col>
+            <v-col md="3" cols="12">
+                <v-btn to="#" color="primary">Buy Now</v-btn>
+                <v-btn to="#" color="second">Add to cart</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, params }) {
+    try {
+      let response = await $axios.$get(`/api/products/${params.id}`);
+      return {
+          product: response.product
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
